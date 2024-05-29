270cddc6391b52624ed9726c8abd57f35222591f
Elena <ebe2017@yandex.ru>
2024-05-29 22:03:35 +0400

    difference

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
+import Difference from "./modules/difference";
 
 window.addEventListener('DOMContentLoaded', () => {
     //на основе импортированного класса создаем новый объект, кот будем использовать
@@ -36,4 +37,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
 });
